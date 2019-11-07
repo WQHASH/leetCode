39b43a5acef7d1a34e689a388e737f9afde58110
wangqi <wangqihash@163.com>
2019-11-07 15:27:11 +0800

    【400W内找出1出现的次数】

---
+++ Tinker/Interview/src/moduleList/subscrible-muyiyang/everyday-one-title/121.js
@@ -0,0 +1,36 @@
+/*
+ * @Description: 第 121 题：统计 1 ~ n 整数中出现 1 的次数
+ * @Author: wangqi
+ * @Date: 2019-10-08 21:36:36
+ * @LastEditTime: 2019-11-07 15:05:54
+ */
+
+let everydayOneTitle121 = "第 121 题：统计 1 ~ n 整数中出现 1 的次数";
+
+{
+    /****
+     *   第 121 题：统计 1 ~ n 整数中出现 1 的次数
+     *          例如统计 1 ~ 400W 出现 1 的次数。
+     *         
+     */
+
+
+
+    function countShow() {
+        var num = 0;
+        for (let i = 1; i < 400000; i++) {
+            if (i) {
+                if (String(i).includes(1)) {
+                    num++;
+                }
+            }
+        }
+        return num
+    };
+
+    console.log(countShow());
+}
+
+export {
+    everydayOneTitle121
+}
--- Tinker/Interview/src/moduleList/subscrible-muyiyang/index.js
+++ Tinker/Interview/src/moduleList/subscrible-muyiyang/index.js
@@ -3,7 +3,7 @@
  * @Author: wangqi
  * @Date: 2019-09-30 16:01:14
 <<<<<<< HEAD
- * @LastEditTime: 2019-11-05 21:27:24
+ * @LastEditTime: 2019-11-07 15:01:48
 =======
  * @LastEditTime: 2019-10-29 21:05:06
 >>>>>>> eae96307166e46dec4c31bd92bf7d6056add5694
@@ -57,4 +57,5 @@ import { everydayOneTitle112 } from "./everyday-one-title/112"
 import { everydayOneTitle113 } from "./everyday-one-title/113"
 import { everydayOneTitle114 } from "./everyday-one-title/114"
 import { everydayOneTitle116 } from "./everyday-one-title/116"
+import { everydayOneTitle121 } from "./everyday-one-title/121"
 export { assign }
